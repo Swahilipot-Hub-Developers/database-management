ef73605e3c7d813cad25f93dab36457bb6e28079
musungui <leshanmsungui@gmail.com>
2023-11-18 22:30:35 +0300

    Added the tables for all the projects

--- projects/cdms_app/admin.py
+++ projects/cdms_app/admin.py
@@ -1,19 +1,35 @@
-# Import the admin module and the Blog model
+# Import the admin module 
 from django.contrib import admin
-from .models import Blog,Member
+# Import the Member, Event,Blog and Artist model fom models.py
+from .models import Member,Event,Blog,Artist,Feedback
+
+
 
 # Define a new class that inherits from admin.ModelAdmin
-class BlogAdmin(admin.ModelAdmin):
-    # Define the fields to display in the table
-    list_display = ('title', 'content', 'status', 'category', 'writer', 'created_at', 'updated_at')
-
-# Register the Blog model with the BlogAdmin class
-admin.site.register(Blog, BlogAdmin)
-
-
 class MemberAdmin(admin.ModelAdmin):
+    # Define the fields to display in the table
     list_display = ('member_id', 'name', 'gender', 'year_of_birth', 'phone_number', 'email_address', 'country', 'county', 'sub_county')
-
+# Register the Member model with the MemberAdmin class
 admin.site.register(Member, MemberAdmin)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display=('id','title','content','status','category','writer','created_at','updated_at')
+admin.site.register(Blog,BlogAdmin)
+
+
+class ArtisAdmin(admin.ModelAdmin):
+    list_display=('id','name','type','description','photo')
+admin.site.register(Artist,ArtisAdmin)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display=('id','first_name','last_name','email','category','message','attachment','timestamp')
+admin.site.register(Feedback,FeedbackAdmin)
+
+
+
+class EventsAdmin(admin.ModelAdmin):
+    list_display=('name','venue','date','time','event_type','description')
+
+admin.site.register(Event,EventsAdmin)
 
 
--- projects/cdms_app/models.py
+++ projects/cdms_app/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Blog (models.Model):
+class Blog(models.Model):
+   id = models.AutoField(primary_key=True)
    title = models.CharField(max_length=200)
    content = models.TextField()
    status = models.CharField(max_length=50)
    category = models.CharField()
    writer = models.CharField(max_length=100)
    created_at = models.DateTimeField(auto_now=True)
-   updated_at = models.DateTimeField(auto_now=True)
+   updated_at = models.DateTimeField(auto_now=True,editable=False)
 
 class Member(models.Model):
     member_id = models.AutoField(primary_key=True)
@@ -20,6 +21,33 @@ class Member(models.Model):
     country = models.CharField(max_length=50, default='Kenya')
     county = models.CharField(max_length=50)
     sub_county = models.CharField(max_length=50)
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='Attachments/artist_photos/', null=False, blank=False)
+
+class Event(models.Model):
+    name = models.CharField(max_length=100,primary_key=True)
+    venue= models.CharField(max_length=100)
+    date=models.DateField()
+    time=models.TimeField()
+    event_type = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Feedback(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    category = models.CharField(max_length=255)
+    message = models.TextField()
+    attachment = models.FileField(upload_to='Attachments/feedback_uploads/', null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True, editable=False)
+
+
+
+
 
 
 
