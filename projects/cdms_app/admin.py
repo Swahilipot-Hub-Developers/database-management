816d3f250a44e61066402f758b2000c68e966d11
musungui <leshanmsungui@gmail.com>
2023-11-19 18:48:57 +0300

    Added data import and export property

--- projects/cdms_app/admin.py
+++ projects/cdms_app/admin.py
@@ -2,32 +2,37 @@
 from django.contrib import admin
 # Import the Member, Event,Blog and Artist model fom models.py
 from .models import Member,Event,Blog,Artist,Feedback
+from import_export.admin import ImportExportModelAdmin
 
 # Define a new class that inherits from admin.ModelAdmin
-class MemberAdmin(admin.ModelAdmin):
+class MemberAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     # Define the fields to display in the table
-    list_display = ('member_id', 'name', 'gender', 'year_of_birth', 'phone_number', 'email_address', 'country', 'county', 'sub_county')
+    list_display = ('id', 'name', 'gender', 'year_of_birth', 'phone_number', 'email_address', 'country', 'county', 'sub_county')
 # Register the Member model with the MemberAdmin class
 admin.site.register(Member, MemberAdmin)
 
-class BlogAdmin(admin.ModelAdmin):
+class BlogAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     list_display=('id','title','content','status','category','writer','created_at','updated_at')
 admin.site.register(Blog,BlogAdmin)
 
 
-class ArtisAdmin(admin.ModelAdmin):
+class ArtisAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     list_display=('id','name','type','description','photo')
 admin.site.register(Artist,ArtisAdmin)
 
-class FeedbackAdmin(admin.ModelAdmin):
+class FeedbackAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     list_display=('id','first_name','last_name','email','category','message','attachment','timestamp')
 admin.site.register(Feedback,FeedbackAdmin)
 
 
 
-class EventsAdmin(admin.ModelAdmin):
-    list_display=('name','venue','date','time','event_type','description')
+class EventsAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    list_display=('id','name','venue','date','time','event_type','description')
 
 admin.site.register(Event,EventsAdmin)
+
+
+
+
 
 
